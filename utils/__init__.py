9da5754909d9c30644cef5d5463cede371ea9e76
huanghao <workhaohuang@126.com>
2021-05-28 09:56:06 +0800

    origin version

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py    
+@Contact :   519605144@qq.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2021/5/28 9:44   huanghao      1.0         None
+'''
